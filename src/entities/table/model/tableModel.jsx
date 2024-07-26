f2289f52e81236c42a66966d28a07a24599535b7
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-26 12:36:53 +0300

    refactor: remove unused code

--- src/entities/table/model/tableModel.jsx
+++ src/entities/table/model/tableModel.jsx
@@ -1,6 +1,6 @@
 import Table from "../table";
 
-const TableModel = ({ users = {}, source }) => {
+const TableModel = ({ users = {}}) => {
   const columnTitles = ["фио", "возраст", "пол", "номер телефона", "адрес"];
   function renderTableColumnTitles(titles) {
     if (titles.length > 0) {
@@ -13,69 +13,38 @@ const TableModel = ({ users = {}, source }) => {
       );
     }
   }
-  function renderDataFromDummy(users) {
-    return users.map((user) => {
-      const { firstName, lastName, age, gender, phone, address } = user;
-      const city = address.city;
-      const street = address.address;
-      return (
-        <tr key={user.id}>
-          <td>
-            {firstName}
-            <br />
-            {lastName}
-          </td>
-          <td>{age}</td>
-          <td>{gender}</td>
-          <td>{phone}</td>
-          <td>
-            {city}
-            <br />
-            {street}
-          </td>
-        </tr>
-      );
-    });
-  }
-  function renderDataFromJSONph(users) {
-    return users.map((user) => {
-      const { name, id: age, username: gender, phone, address } = user;
-      const city = address.city;
-      const street = address.street;
-      const [firstName, lastName] = name.split(" ");
-      return (
-        <tr key={user.id}>
-          <td>
-            {firstName}
-            <br />
-            {lastName}
-          </td>
-          <td>{age}</td>
-          <td>{gender}</td>
-          <td>{phone}</td>
-          <td>
-            {city}
-            <br />
-            {street}
-          </td>
-        </tr>
-      );
-    });
-  }
-  function renderTableRows(users, dataSource) {
-    if (Object.keys(users).length > 0) {
-      if (dataSource === "JSONph") {
-        return renderDataFromJSONph(users);
-      } else {
-        return renderDataFromDummy(users);
-      }
+  function renderData(users) {
+    if(Object.keys(users).length > 0) {
+      return users.map((user) => {
+        const { firstName, lastName, age, gender, phone, address } = user;
+        const city = address.city;
+        const street = address.address;
+        return (
+          <tr key={user.id}>
+            <td>
+              {firstName}
+              <br />
+              {lastName}
+            </td>
+            <td>{age}</td>
+            <td>{gender}</td>
+            <td>{phone}</td>
+            <td>
+              {city}
+              <br />
+              {street}
+            </td>
+          </tr>
+        );
+      });
     }
   }
+  
   return (
     <>
       <Table
         header={renderTableColumnTitles(columnTitles)}
-        body={renderTableRows(users, source)}
+        body={renderData(users)}
       />
     </>
   );
